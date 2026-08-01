73acad93c24d5bcef96b0727e02b7695277eee86
medalzz <unkinkedmedal94@gmail.com>
2026-08-01 17:02:26 -0300

    laravel api v1 initial route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function (): void {
+    // Route::post('/login', [AuthController::class, 'login']);
+
+    // Route::get('/users', function () {
+    //     return DB::table('users')
+    //         ->get()
+    //         ->toJson();
+    // });
+
+    Route::middleware('auth:sanctum')->group(function (): void {
+        // Route::post('/logout', [AuthController::class, 'logout']);
+
+        // Route::get('/user', function (Request $request) {
+        //     return $request->user();
+        // });
+
+        // Route::apiResource('projects', ProjectController::class);
+    });
+});
