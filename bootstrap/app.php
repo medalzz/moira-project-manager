be34396dce8e5927d8b107e6025ae90d57734cc7
medalzz <unkinkedmedal94@gmail.com>
2026-08-02 11:54:07 -0300

    fix non-api routes not rendering json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
-    })->create();
+        $exceptions->shouldRenderJsonWhen(fn (): bool => true);
+    })
+    ->create();
