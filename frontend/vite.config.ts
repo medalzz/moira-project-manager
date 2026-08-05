cd7508bac2e8a90468edb692d8fb9aa2f00ef749
medalzz <unkinkedmedal94@gmail.com>
2026-08-05 13:35:53 -0300

    add vue proxy to laravel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,13 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(), 
-        vueDevTools()
-    ],
+    plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+
+        proxy: {
+            '/api': {
+                target: 'http://nginx',
+                changeOrigin: true,
+            },
         },
     },
 })
