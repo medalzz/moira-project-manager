04a1c910bc0b6b16fbd2be0eae3e9daf071b8b12
medalzz <unkinkedmedal94@gmail.com>
2026-08-15 01:23:27 -0300

    fix wrong frontend settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,19 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'http://localhost:5173',
+        cors: {
+            origin: 'http://localhost:8000',
+        },
+        hmr: {
+            host: 'localhost',
+        },
+    },
+
     plugins: [
         laravel({
             input: 'frontend/app.ts',
