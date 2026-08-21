ec4d0edb993b9cd01e6d93878de4bcd8d27ece55
medalzz <unkinkedmedal94@gmail.com>
2026-08-21 20:20:22 -0300

    translate update password

--- frontend/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ frontend/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -39,18 +39,18 @@ const updatePassword = () => {
     <section>
         <header>
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                Update Password
+                {{ $t('update_password') }}
             </h2>
 
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            <!-- <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                 Ensure your account is using a long, random password to stay
                 secure.
-            </p>
+            </p> -->
         </header>
 
         <form @submit.prevent="updatePassword" class="mt-6 space-y-6">
             <div>
-                <InputLabel for="current_password" value="Current Password" />
+                <InputLabel for="current_password" :value="$t('current_password')" />
 
                 <TextInput
                     id="current_password"
@@ -68,7 +68,7 @@ const updatePassword = () => {
             </div>
 
             <div>
-                <InputLabel for="password" value="New Password" />
+                <InputLabel for="password" :value="$t('new_password')" />
 
                 <TextInput
                     id="password"
@@ -85,7 +85,7 @@ const updatePassword = () => {
             <div>
                 <InputLabel
                     for="password_confirmation"
-                    value="Confirm Password"
+                    :value="$t('confirm_password')"
                 />
 
                 <TextInput
@@ -103,7 +103,7 @@ const updatePassword = () => {
             </div>
 
             <div class="flex items-center gap-4">
-                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+                <PrimaryButton :disabled="form.processing">{{ $t('save') }}</PrimaryButton>
 
                 <Transition
                     enter-active-class="transition ease-in-out"
@@ -115,7 +115,7 @@ const updatePassword = () => {
                         v-if="form.recentlySuccessful"
                         class="text-sm text-gray-600 dark:text-gray-400"
                     >
-                        Saved.
+                        {{ $t('saved') }}
                     </p>
                 </Transition>
             </div>
